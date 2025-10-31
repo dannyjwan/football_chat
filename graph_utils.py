922a69e2a5af056b4c17e96c31f33dc8621c69e1
Danny Wan <73850299+dannyjwan@users.noreply.github.com>
2025-10-31 00:49:15 -0400

    Fix import: GraphCypherQAChain from langchain.chains; simplify chain params

--- graph_utils.py
+++ graph_utils.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_community.chains import GraphCypherQAChain
+from langchain.chains import GraphCypherQAChain
 from langchain_community.graphs import Neo4jGraph
 from langchain_openai import ChatOpenAI
 
@@ -12,11 +12,9 @@ def initialize_resources(api_key: str):
     )
     graph.refresh_schema()
     chain = GraphCypherQAChain.from_llm(
-        llm=ChatOpenAI(api_key=api_key, model="gpt-4o-mini", temperature=0), 
+        llm=ChatOpenAI(api_key=api_key, model="gpt-4o-mini", temperature=0),
         graph=graph,
         verbose=True,
-        show_intermediate_steps=True,
-        allow_dangerous_content=True,
         )
     return graph, chain
 
